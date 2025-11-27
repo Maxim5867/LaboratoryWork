52feb895b7e85622200bd125514c15094b46862a
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2025-11-27 12:36:59 +0300

    Update N9.java

--- lab5_3/N9.java
+++ lab5_3/N9.java
@@ -1,39 +1,52 @@
+package lab5_3;
 import java.util.Scanner;
-
 public class N9 {
     public static void main(String[] args) {
         Scanner consolscanner = new Scanner(System.in);
-        int N = 1;
+        int N = 100;
         String center;
         String point;
-        boolean index;
-        boolean index1;
+        int index = 1;
+        int indexx = 1;
+        int indexy = 1;
+        int index1 = 1;
+        int index1x = 1;
+        int index1y = 1;
+
         do {
-            System.out.print("Введите координаты центра окружности в формате x:y\n->");
+            System.out.print("Введите координаты центра окружности в формате (x,y)\n->");
             center = consolscanner.nextLine();
-            index = center.contains(":");
-            if (!index) {
+            index = center.indexOf(",");
+            indexx = center.indexOf("(");
+            indexy = center.indexOf(")");
+            if (index == -1 || indexx == -1 || indexy == -1) {
                 System.out.println("Неправильный ввод!");
             }
-        } while (!index);
+        } while (index == -1 || indexx == -1 || indexy == -1);
+
         do {
-            System.out.print("Введите координаты точки окружности x:y\n->");
+            System.out.print("Введите координаты точки окружности (x,y)\n->");
             point = consolscanner.nextLine();
-            index1 = point.contains(":");
-            if (!index1) {
+            index1 = point.indexOf(",");
+            index1x = point.indexOf("(");
+            index1y = point.indexOf(")");
+            if (index1 == -1 || index1x == -1 || index1y == -1) {
                 System.out.println("Неправильный ввод!");
             }
-        } while (!index1);
-        int x1 = Integer.parseInt(center.split(":")[0])*N;
-        int y1 = Integer.parseInt(center.split(":")[1])*N;
-        int x2 = Integer.parseInt(point.split(":")[0])*N;
-        int y2 = Integer.parseInt(point.split(":")[1])*N;
-        perimeter(x1,y1,x2,y2);
+        } while (index1 == -1 || index1x == -1 || index1y == -1);
+
+        String center1 = center.replace("(","").replace(")","");
+        String point1 = point.replace("(","").replace(")","");
+        int x1 = Integer.parseInt(center1.split(",")[0])*N;
+        int y1 = Integer.parseInt(center1.split(",")[1])*N;
+        int x2 = Integer.parseInt(point1.split(",")[0])*N;
+        int y2 = Integer.parseInt(point1.split(",")[1])*N;
+        System.out.printf("%.2f", perimeter(x1,y1,x2,y2));
     }
 
-    static void perimeter (int a, int b, int c, int d){
+    static double perimeter (int a, int b, int c, int d){
         double length = 2 * Math.PI * (Math.sqrt(Math.pow(c-a,2) + Math.pow(d-b,2)));
-        System.out.printf("%.2f", length);
+        return length;
 
     }
 }
